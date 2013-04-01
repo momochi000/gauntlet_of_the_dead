5b01fb45ad3b2234b2a2d6f302ceae9d52839b74
Zachery Chin <zacherychin@yahoo.com>
2013-04-01 10:36:44 -0700

    Stub out a welcome page using jade at the root url.
    [#47114015]

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
-var express = require('express');
+var app, 
+  express, jade, 
+  port;
+express = require('express');
 
-var app = express.createServer(express.logger());
+app = express();
+
+app.set("view engine", "jade");
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render("index");
 });
 
-var port = process.env.PORT || 5000;
+//app.get('/index', function(request, response) {
+//  response.render('index', function (err, html){
+//    console.log("DEBUG: in the index render call");
+//    console.log(err);
+//    console.log(html);
+//  });
+//});
+
+port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
