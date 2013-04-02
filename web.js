86aebd5b4c3c5fb77d94c99f67d6caf7d2792c4f
Zachery Chin <zacherychin@yahoo.com>
2013-04-01 20:04:03 -0700

    Package nib and include stylus+nib in the app

--- web.js
+++ web.js
@@ -1,23 +1,27 @@
 var app, 
-  express, jade, 
+  express, jade, stylus, nib,
   port;
 express = require('express');
+stylus = require('stylus');
+nib = require('nib');
 
 app = express();
 
 app.set("view engine", "jade");
 
+app.use(express.logger('dev'))
+
+app.use(stylus.middleware({
+  debug: true, 
+  src: __dirname + '/stylesheets',
+  compile: compile
+}));
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   response.render("index");
 });
-
-//app.get('/index', function(request, response) {
-//  response.render('index', function (err, html){
-//    console.log("DEBUG: in the index render call");
-//    console.log(err);
-//    console.log(html);
-//  });
-//});
 
 port = process.env.PORT || 5000;
 app.listen(port, function() {
