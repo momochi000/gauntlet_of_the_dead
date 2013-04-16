62e6088ade809253b6a178d38fd64fc5c210abaf
Zachery Chin <zacherychin@yahoo.com>
2013-04-15 23:03:28 -0700

    add spec directory and a hello test to ensure jasmine is working

---
+++ spec/foo.spec.js
@@ -0,0 +1,7 @@
+// TODO: Throw out this file and replace it with real tests
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
