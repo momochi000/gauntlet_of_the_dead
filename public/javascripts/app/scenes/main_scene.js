005fc0339d2fa5c2a865fca0e4cc53e0396d8833
Zachery Chin <zacherychin@yahoo.com>
2013-06-13 22:55:00 -0700

    Add debug entity to the scene for development

--- public/javascripts/app/scenes/main_scene.js
+++ public/javascripts/app/scenes/main_scene.js
@@ -1,10 +1,10 @@
 Crafty.scene('main', function (){
-  var camera, entities;
+  var camera, entities, debug;
   Gauntlet.Sprites.load();
   Crafty.background("black");
   Gauntlet.Maps.MainMap.generateMap();
   entities = Gauntlet.Maps.MainMap.generateEntities();
   camera = Gauntlet.Maps.MainMap.generateCamera();
-
+  debug = Gauntlet.Entities.Debug.create(camera);
   camera.followTarget(entities['player']);
 });
